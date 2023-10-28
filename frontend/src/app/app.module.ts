a0a048c59a6ce08cece37a77fdf327e0b3c184d1
Kaleb White <kalebw02@ad.unc.edu>
2023-10-28 15:02:36 -0400

    Add some basic card / angular material functionality

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,17 +6,21 @@ import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { ContentAreaComponent } from './content-area/content-area.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
-    SideBarComponent
+    SideBarComponent,
+    ContentAreaComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
